ba0b09b60cf1929e3e2b9618933a14abd23581f7
HuynhPPP <tankhuong02@gmail.com>
2025-04-20 14:12:48 +0700

    fix + add migration file

---
+++ database/migrations/2025_04_19_140043_create_site_settings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('site_settings', function (Blueprint $table) {
+      $table->id();
+      $table->string('logo')->nullable();
+      $table->string('phone')->nullable();
+      $table->string('email')->nullable();
+      $table->string('address')->nullable();
+      $table->string('facebook')->nullable();
+      $table->string('youtube')->nullable();
+      $table->string('copyright')->nullable();
+      $table->timestamps();
+    });
+    DB::table('site_settings')->insert([
+      'logo' => 'logo.png',
+      'phone' => '0369455664',
+      'email' => 'hotro@gmail.com',
+      'address' => '256 Nguyễn Văn Cừ, Quận Ninh Kiều, Thành phố Cần Thơ',
+      'facebook' => 'https://www.facebook.com/dvk0322',
+      'youtube' => 'https://www.youtube.com/channel/UCE8VN4o0s6lBHN3QGMAVgXQ',
+      'copyright' => 'Copyright © 2007 - 2025 FindHouse',
+    ]);
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('site_settings');
+  }
+};
